eb8dc26b2866e5aefb8ed2703943739c162df417
resiyandawafa02 <75015733+reiiiofsunshine1@users.noreply.github.com>
2023-06-03 15:28:26 +0700

    [added] profile page

--- profile.php
+++ profile.php
@@ -16,26 +16,26 @@
 </head>
 <body>
     <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="index.php">BluBook</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" 
-                    id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" 
-                        aria-current="page" 
-                        href="index.php">Store</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link active"
-                        aria-current="page"
-                        href="about.php">About</a>
-                    </li>
-                    <li class="nav-item">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="index.php">BluBook</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" 
+		         id="navbarSupportedContent">
+		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		        <li class="nav-item">
+		          <a class="nav-link active" 
+		             aria-current="page" 
+		             href="index.php">Home</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link active"
+				  	aria-current="page"
+		             href="about.php">About</a>
+		        </li>
+                <li class="nav-item">
 		          <?php if (isset($_SESSION['user_id'])) {?>
 		          	<a class="nav-link" 
 		             href="profile.php">Profile</a>
@@ -43,25 +43,22 @@
 		          <a class="nav-link" 
 		             href="login.php">Login</a>
 		          <?php } ?>
-                
 		        </li>
-                </ul>
+		      </ul>
 		    </div>
 		  </div>
-	</nav>
-    <section class="about-us">
-        <div class="about">
+		</nav>
+        <section class="Profile">
+        <div class="profile">
         <!--<img src="girl.png" class="pic">-->
         <div class="text">
-            <h2>About Us</h2>
-            <h5>BluBook<span> Your e-Library</span></h5>
+            <h2>Profile</h2>
+            <h5>Your<span> Name</span></h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae cumque voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
             <div class="data">
-            <a href="#" class="hire">Visit us soon!</a>
             </div>
         </div>
         </div>
     </section>
     </div>
 </body>
-</html>
