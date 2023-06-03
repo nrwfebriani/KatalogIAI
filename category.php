22e6022d993d3c81dd42855349ded8f95f700a8d
resiyandawafa02 <75015733+reiiiofsunshine1@users.noreply.github.com>
2023-06-03 15:12:32 +0700

    [change] font hehe

--- category.php
+++ category.php
@@ -47,7 +47,7 @@ $current_category = get_category($conn, $id);
 	<div class="container">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		  <div class="container-fluid">
-		    <a class="navbar-brand" href="index.php">Online Book Store</a>
+		    <a class="navbar-brand" href="index.php">BluBook</a>
 		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		      <span class="navbar-toggler-icon"></span>
 		    </button>
